ea0610733e4f4fc2ae4b753d36d472101b1c4c4e
larry <larrymcu@users.noreply.github.com>
2019-11-30 18:51:59 +0800

    20191130

---
+++ ur0ssl.js
@@ -0,0 +1,2 @@
+setTimeout("ur3ssl_in()",0)
+function ur3ssl_in(){if(window.location.protocol!=='https:'){window.location='https://'+window.location.hostname+window.location.pathname+window.location.hash;}}
