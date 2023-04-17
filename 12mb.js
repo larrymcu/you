2c2956dc37b932cd7f3f710e48e0171c3721c169
larry <larrymcu@users.noreply.github.com>
2023-04-17 11:24:14 +0800

    20230417

--- 12mb.js
+++ 12mb.js
@@ -3,43 +3,57 @@
 */
 
 var imageAddr = "12MB.jpg" + "?n=" + Math.random();
-var startTime, endTime;
-var downloadSize = 13140191; //12.5Mb
-var download = new Image();
-var roundedDecimals = 2;
+/*var downloadSize = 45554906; //43.4Mb
+*/
 var bytesInAKilobyte = 1024;
+var roundedDecimals = 2;
 
-function showResults() {
-  var duration = (endTime - startTime) / 1000;
-  var bitsLoaded = downloadSize * 8;
-  var speedBps = ( bitsLoaded / duration ).toFixed(roundedDecimals);
-  var displaySpeed = speed(speedBps);
-  var results = "<h2>你的网速测试为<h2><h1>" + displaySpeed.value + " " + displaySpeed.units + "</h1>"
-    
-    $('#results').fadeOut('fast',function(){
-      $('#results').html(results);
-      $('#results').fadeIn('fast', function(){
-        $('#starttest').text('完成');
-      });
-    });
+function showResults(speedBps) {
+  var KBps = (speedBps / bytesInAKilobyte *10).toFixed(roundedDecimals);
+  var MBps = (KBps / bytesInAKilobyte).toFixed(roundedDecimals);
+  var displaySpeed = MBps > 1 ? {value: MBps, units: "Mbit/s"} : {value: KBps, units: "Kbit/s"};
+  var results = "<h2>你的网速测试为<h2><h1>" + displaySpeed.value + " " + displaySpeed.units + "</h1>";
+  $('#results').html(results);
 }
 
-function speed( bitsPerSecond ){
-  var KBps = (bitsPerSecond / bytesInAKilobyte).toFixed(roundedDecimals);
-  if ( KBps <= 1 ) return { value: bitsPerSecond, units: "bit/s" };
-  var MBps = (KBps / bytesInAKilobyte).toFixed(roundedDecimals);
-  if ( MBps <= 1 ) return { value: KBps, units: "Kbit/s" };
-  else return { value: MBps, units: "Mbit/s" };
+function updateProgress(evt) {
+  if (evt.lengthComputable) {
+    var percentage = Math.round(evt.loaded / evt.total * 100);
+    $('#progressbar').attr('value', percentage);
+    var duration = (new Date().getTime() - startTime) / 1000;
+    var speedBps = evt.loaded / duration * 8;
+			var displaySpeed = speedBps / (bytesInAKilobyte * bytesInAKilobyte);
+
+			if (displaySpeed > 1) {
+			  displaySpeed = displaySpeed.toFixed(roundedDecimals);
+			  $('#speed').text(displaySpeed + ' Mbit/s');
+			} else {
+			  displaySpeed = (speedBps / bytesInAKilobyte).toFixed(roundedDecimals);
+			  $('#speed').text(displaySpeed + ' Kbit/s');
+			}
+  }
+}
+
+function startTest() {
+  $('#starttest').text('测试下载速度...');
+  $('#starttest').attr('disabled', 'disabled');
+  $('#progressbar').attr('value', 0);
+  $('#speed').text('');
+  var xhr = new XMLHttpRequest();
+  xhr.onprogress = updateProgress;
+  xhr.onload = function () {
+    var speedBps = this.response.byteLength / ((new Date().getTime() - startTime) / 1000);
+    showResults(speedBps);
+    $('#starttest').removeAttr('disabled');
+	$('#speed').text('');
+    $('#starttest').text('完成');
+  };
+  xhr.open('GET', imageAddr, true);
+  xhr.responseType = 'arraybuffer';
+  xhr.send();
 }
 
 $('#starttest').on('click', function(){
-  $('#starttest').text('测试下载速度...');
-  $('#starttest').attr('disabled', 'disabled');
-  
-  download.onload = function () {
-    endTime = (new Date()).getTime();
-    showResults();
-  }
-  startTime = (new Date()).getTime();
-  download.src = imageAddr;
-})
+  startTime = new Date().getTime();
+  startTest();
+});
